a374c2a032e700c3b7b5b6f3a58034f9ab2394d4
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2021-02-18 20:23:26 +0100

    new best

--- estimations/exploration_peisenha_parttime/construct_observed_components.py
+++ estimations/exploration_peisenha_parttime/construct_observed_components.py
@@ -9,7 +9,7 @@ from dev_library import df_alignment
 from moments import get_moments
 
 fname_data = os.environ["PROJECT_DIR"] + "/resources/soepcore_struct_prep.dta"
-df_obs = df_alignment(pd.read_stata(fname_data, convert_categoricals=False))
+df_obs = df_alignment(pd.read_stata(fname_data, convert_categoricals=False), is_obs=True)
 pkl.dump(get_weighting_matrix(df_obs, get_moments, 500), open("weighting_matrix.pkl", "wb"))
 pkl.dump(get_moments(df_obs), open("observed_moments.pkl", "wb"))
 
