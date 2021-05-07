a1285aed216c837ea8da51537a8af39c34b89ed9
peisenha <peiseha@protonmail.com>
2021-05-07 12:55:51 +0200

    edits

--- estimations/basecamp/construct_observed_components.py
+++ estimations/basecamp/construct_observed_components.py
@@ -10,7 +10,7 @@ from moments import get_moments
 fname_data = os.environ["DATA_DIR"] + "/df-observed-data-unweighted.pkl"
 df_obs_weighted = pd.read_pickle(fname_data)
 
-info = get_weighting_matrix(df_obs_weighted, get_moments, 100)
+info = get_weighting_matrix(df_obs_weighted, get_moments, 500)
 pkl.dump(info, open("weighting-matrix.pkl", "wb"))
 
 info = get_moments(df_obs_weighted)
--- estimations/basecamp/run.py
+++ estimations/basecamp/run.py
@@ -10,11 +10,11 @@ from moments import get_moments
 df_start = pd.read_pickle("start.soepy.pkl")
 
 adapter_kwargs = dict()
-adapter_kwargs["weighting_matrix"] = np.identity(1301)#pkl.load(open("weighting-matrix.pkl", "rb"))
+adapter_kwargs["weighting_matrix"] = pkl.load(open("weighting-matrix.pkl", "rb"))
 adapter_kwargs["model_spec_init_file_name"] = "resources/model_spec_init.yml"
 adapter_kwargs["moments_obs"] = pkl.load(open("observed-moments.pkl", "rb"))
 adapter_kwargs["get_moments"] = get_moments
 adapter_kwargs["params"] = df_start
 
 adapter_smm = SimulationBasedEstimationCls(**adapter_kwargs)
-np.testing.assert_almost_equal(adapter_smm.fval, 2366.4468131697445)
+np.testing.assert_almost_equal(adapter_smm.fval, 11648.215716656949)
