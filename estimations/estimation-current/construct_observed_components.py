bd1128a1da5253cbd96cff40a3c7e6236d779499
peisenha <peiseha@protonmail.com>
2021-05-07 20:51:02 +0200

    fix moments

--- estimations/estimation-current/construct_observed_components.py
+++ estimations/estimation-current/construct_observed_components.py
@@ -4,15 +4,18 @@ import pickle as pkl
 import pandas as pd
 
 from dev_library import get_weighting_matrix
-from dev_library import df_alignment
 
 from moments import get_moments
 
-fname_data = os.environ["PROJECT_DIR"] + "/resources/soepcore_struct_prep.dta"
-df_obs = df_alignment(pd.read_stata(fname_data, convert_categoricals=False))
+# We use the unweighted sample for the weighting matrix and then the weighted for the descriptives.
+fname_data = os.environ["DATA_DIR"] + "/df-observed-data-unweighted.pkl"
+df_obs_unweighted = pd.read_pickle(fname_data)
 
-info = get_weighting_matrix(df_obs, get_moments, 500)
+info = get_weighting_matrix(df_obs_unweighted, get_moments, 500)
 pkl.dump(info, open("weighting-matrix.pkl", "wb"))
 
-info = get_moments(df_obs)
+fname_data = os.environ["DATA_DIR"] + "/df-observed-data-weighted-subsample.pkl"
+df_obs_weighted = pd.read_pickle(fname_data)
+
+info = get_moments(df_obs_weighted)
 pkl.dump(info, open("observed-moments.pkl", "wb"))
