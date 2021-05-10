edc89014d711376e7a1c8588e127abefaa55c148
peisenha <peiseha@protonmail.com>
2021-05-10 17:36:42 +0200

    edits

--- estimations/estimation-current/run.py
+++ estimations/estimation-current/run.py
@@ -25,7 +25,7 @@ adapter_kwargs["get_moments"] = get_moments
 adapter_kwargs["params"] = df_start
 
 adapter_smm = SimulationBasedEstimationCls(**adapter_kwargs)
-np.testing.assert_almost_equal(adapter_smm.fval, 6024.894300756455)
+np.testing.assert_almost_equal(adapter_smm.fval, 3856.281686510786)
 
 # We set the tuning parameters of the optimizer so that it runs forever.
 opt_kwargs = dict()
@@ -37,4 +37,4 @@ opt_kwargs["maxfun"] = 1
 x0, bounds = prepare_optimizer_interface(df_start)
 p_wrapper_numpy = partial(wrapper_numpy, df_start, adapter_smm)
 rslt = pybob.solve(p_wrapper_numpy, x0, bounds=bounds, **opt_kwargs)
-np.testing.assert_almost_equal(rslt.f, 6024.894300756455)
+np.testing.assert_almost_equal(rslt.f, 3856.281686510786)
