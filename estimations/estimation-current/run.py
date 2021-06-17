4750874c6243ee3f7febacb5c356d84c7203462d
peisenha <eisenhauer@policy-lab.org>
2021-06-17 15:20:19 +0200

    updated exog

--- estimations/estimation-current/run.py
+++ estimations/estimation-current/run.py
@@ -24,7 +24,7 @@ adapter_kwargs["get_moments"] = get_moments
 adapter_kwargs["params"] = df_start
 
 adapter_smm = SimulationBasedEstimationCls(**adapter_kwargs)
-np.testing.assert_almost_equal(adapter_smm.fval, 4238.1620040064445)
+np.testing.assert_almost_equal(adapter_smm.fval, 4355.076111586377)
 
 # We set the tuning parameters of the optimizer so that it runs forever.
 opt_kwargs = dict()
@@ -36,4 +36,4 @@ opt_kwargs["maxfun"] = 1
 x0, bounds = prepare_optimizer_interface(df_start)
 p_wrapper_numpy = partial(wrapper_numpy, df_start, adapter_smm)
 rslt = pybob.solve(p_wrapper_numpy, x0, bounds=bounds, **opt_kwargs)
-np.testing.assert_almost_equal(rslt.f, 4238.1620040064445)
+np.testing.assert_almost_equal(rslt.f, 4355.076111586377)
